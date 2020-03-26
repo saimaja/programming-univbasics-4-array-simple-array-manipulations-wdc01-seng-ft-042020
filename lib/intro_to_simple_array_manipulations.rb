9b6fb6190d9d11fc3b8897289955f38812b69f49
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-26 22:55:27 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+=begin
+def using_pop(array)
+  last_element = array.pop()
+  return last_element
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  =end
+  
