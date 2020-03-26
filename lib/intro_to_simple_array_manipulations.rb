29be2c024d0fd671248307fcddb57d92e9ae68aa
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-26 23:09:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,19 @@ def pop_with_args(array)
   array.pop(2)
 end
   
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element[3])
+end
+
